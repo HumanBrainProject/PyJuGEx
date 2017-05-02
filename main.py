46a37bb7c838d515818116331a8f653e2496685c
Haimasree Bhattacharya <h.bhattacharya@fz-juelich.de>
2017-05-02 17:20:26 +0200

    Basic setup

---
+++ main.py
@@ -0,0 +1,36 @@
+# In this example we are going to create a simple HTML
+# page with 2 input fields (numbers), and a link.
+# Using jQuery we are going to send the content of both
+# fields to a route on our application, which will
+# sum up both numbers and return the result.
+# Again using jQuery we'l show the result on the page
+
+
+# We'll render HTML templates and access data sent by GET
+# using the request object from flask. jsonigy is required
+# to send JSON as a response of a request
+from flask import Flask, render_template, request, jsonify, json
+import backend
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route that will process the AJAX request, sum up two
+# integer numbers (defaulted to zero) and return the
+# result as a proper JSON response (Content-Type, etc.)
+@app.route('/_jugex')
+def jugex():
+    #a = request.args.get('a')
+    #b = request.args.get('b')
+    res = backend.performJugex()
+    return jsonify(result=res)
+
+if __name__ == '__main__':
+    debug = true
---
+++ static/performJugex.js
@@ -0,0 +1,35 @@
+$(function() {
+    $('a#calculate').bind('click', function() {
+        $.getJSON('/_jugex', {
+                      //      a: $('input[name="a"]').val(),
+                      //      b: $('input[name="b"]').val()
+                  }, function(data) {
+                      var json = JSON.parse(data.result);
+                      for(var i=0; i < json.genes.length; i++)
+                          console.log(json.genes[i].name+' '+json.genes[i].pval);
+                      var table = document.createElement("TABLE");
+                      table.border = "1";
+                      var columnCount = 2;
+                      var row = table.insertRow(-1);
+                      var headerCell = document.createElement("TH");
+                      headerCell.innerHTML = 'Name of Gene';
+                      row.appendChild(headerCell);
+                      headerCell = document.createElement("TH");
+                      headerCell.innerHTML = 'P Value';
+                      row.appendChild(headerCell);
+                      for(var i=0; i<json.genes.length; i++){
+                          row = table.insertRow(-1);
+                          cell = row.insertCell(-1);
+                          cell.innerHTML = json.genes[i].name;
+                          cell = row.insertCell(-1);
+                          cell.innerHTML = json.genes[i].pval;
+                      }
+                      var dvTable = document.getElementById("dvTable");
+                      dvTable.innerHTML = "";
+                      dvTable.appendChild(table);
+
+                      //console.log(data.result);
+                  });
+        return false;
+    });
+});
