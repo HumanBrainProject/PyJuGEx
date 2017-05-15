c6fbaab372cef805f5646d2f5567b1176dfece6e
Haimasree Bhattacharya <h.bhattacharya@fz-juelich.de>
2017-05-15 15:06:43 +0200

    Highlight of rows with differentially expressed genes

--- static/performJugex.js
+++ static/performJugex.js
@@ -15,31 +15,34 @@ $(function() {
                           }
                       }
                       var json = JSON.parse(data.result);
-                      for(var i=0; i < json.genes.length; i++)
-                          console.log(json.genes[i].name+' '+json.genes[i].pval);
+//                      for(var i=0; i < json.genes.length; i++)
+//                          console.log(json.genes[i].name+' '+json.genes[i].pval);
                       var table = document.createElement("TABLE");
-//                      table.style.backgroundColor = "yellow";
-                      table.border = "1";
                       var columnCount = 2;
                       var row = table.insertRow(-1);
                       var headerCell = document.createElement("TH");
-                      headerCell.innerHTML = '    Name of Gene    ';
+                      headerCell.innerHTML = 'Name of Gene';
                       row.appendChild(headerCell);
                       headerCell = document.createElement("TH");
-                      headerCell.innerHTML = '    p value    ';
+                      headerCell.innerHTML = '\u00A0\u00A0\p value';
+                      //headerCell.innerHTML = 'p value';
                       row.appendChild(headerCell);
                       for(var i=0; i<json.genes.length; i++){
                           row = table.insertRow(-1);
                           cell = row.insertCell(-1);
                           cell.innerHTML = json.genes[i].name;
                           cell = row.insertCell(-1);
-                          cell.innerHTML = json.genes[i].pval;
+                          cell.innerHTML = "\u00A0\u00A0";
+                          cell.innerHTML += json.genes[i].pval;
                           if(parseFloat(json.genes[i].pval) < .05)
-                              row.style.backgroundColor = "yellow";
+                              cell.style.color = "blue";
+                          else
+                              cell.style.color = "red";
                       }
                       var dvTable = document.getElementById("dvTable");
                       dvTable.innerHTML = "";
                       dvTable.appendChild(table);
+                      dvTable.style="height:200px; overflow-y: scroll;"
                       //document.getElementById("dvTable").childNodes[0].style.backgroundColor = "yellow";
                   });        
         return false;
