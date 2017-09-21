252d33d4b63bc8f58f903ba4c175ce9411e3a946
Haimasree Bhattacharya <h.bhattacharya@fz-juelich.de>
2017-09-21 09:51:54 +0200

    added comments

--- pyjugex.py
+++ pyjugex.py
@@ -5,25 +5,16 @@ import json
 import csv
 import requests
 
+
 def DifferentialGeneExpression(gene_cache):
     analysisjugex = mymodule.Analysis(gene_cache)
     return analysisjugex
 
-def readCSVFile(filename):
-    rows = dict();
-    rows['probe_id'] = []
-    rows['gene_symbol'] = []
-    rows['entrez_id'] = []
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            rows['probe_id'].append(row['probe_id'])
-            rows['gene_symbol'].append(row['gene_symbol'])
-            rows['entrez_id'].append(int(row['entrez_id']))
-    return rows
-
-
 def ReadParcellations(dictionaryimages, regionname):
+    """
+    Download and save a local copy of the masks
+    for the respective regions
+    """
     url = dictionaryimages[regionname]
     r = requests.get(url, verify=False)
     filename = 'output'+regionname+'.nii.gz'
