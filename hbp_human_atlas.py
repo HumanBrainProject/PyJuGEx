14ceae36cfbdf41c5c873bd5f3d84faedf209a0f
haimasree <31005140+haimasree@users.noreply.github.com>
2017-10-04 16:04:24 +0200

    Update hbp_human_atlas.py

--- hbp_human_atlas.py
+++ hbp_human_atlas.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 import nibabel as nib
 import requests
+from future.standard_library import install_aliases
+install_aliases()
 
 dictionaryimages = {'FP1': 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10m_l_N10_nlin2Stdicbm152casym.nii.gz',
 'FP2': 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10p_l_N10_nlin2Stdicbm152casym.nii.gz'}
 MNI152 = True
 
-def probability_map(regionname, coordspace):
-    url = dictionaryimages[regionname]
-    r = requests.get(url, verify=False)
-    filename = 'output'+regionname+'.nii.gz'
-    with open(filename, 'wb') as f
-        f.write(r.content)
-    return nib.load(filename)
+class jubrain:
+    def probability_map(regionname, coordspace):
+        url = dictionaryimages[regionname]
+        r = requests.get(url, verify=False)
+        filename = 'output'+regionname+'.nii.gz'
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        return nib.load(filename)
