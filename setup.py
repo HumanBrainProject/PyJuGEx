c7eac173292d4b1263e5faef096be99d40441c04
haimasree <31005140+haimasree@users.noreply.github.com>
2017-10-04 15:41:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 setup(name='pyjugex',
       version='1.0',
-      py_modules=['pyjugex', 'analysispyjugex'],
+      py_modules=['pyjugex', 'analysispyjugex', 'hbp_human_atlas'],
       )
