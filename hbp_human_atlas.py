a4aff816764f9ea03b3065521682f5d98091091d
haimasree <31005140+haimasree@users.noreply.github.com>
2017-10-04 15:46:36 +0200

    Update hbp_human_atlas.py

--- hbp_human_atlas.py
+++ hbp_human_atlas.py
@@ -7,11 +7,10 @@ dictionaryimages = {'FP1': 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access
 'FP2': 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10p_l_N10_nlin2Stdicbm152casym.nii.gz'}
 MNI152 = True
 
-class jubrain:
-    def probability_map(regionname, coordspace):
-        url = dictionaryimages[regionname]
-        r = requests.get(url, verify=False)
-        filename = 'output'+regionname+'.nii.gz'
-        with open(filename, 'wb') as f:
-            f.write(r.content)
-        return nib.load(filename)
+def probability_map(regionname, coordspace):
+    url = dictionaryimages[regionname]
+    r = requests.get(url, verify=False)
+    filename = 'output'+regionname+'.nii.gz'
+    with open(filename, 'wb') as f
+        f.write(r.content)
+    return nib.load(filename)
