1c641445602a883155148354c638ddc1237b8eee
haimasree <31005140+haimasree@users.noreply.github.com>
2017-10-06 11:19:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setup(name='pyjugex',
       version='0.1',
       description='Perform differential gene expression on two chosen brain regions' ,
       py_modules=['pyjugex', 'analysispyjugex', 'hbp_human_atlas'],
-      install_requires=['numpy', 'scipy', 'statsmodels', 'requests', 'nibabel', 'xmltodict', 'json',], 
+      install_requires=['numpy', 'scipy', 'statsmodels', 'requests', 'nibabel', 'xmltodict',], 
       )
