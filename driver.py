91fdc4bf11ceb451e4c8b6e7969f7a4d2075544f
haimasree <hbhattacharya@fz-juelich.de>
2017-11-03 15:46:24 +0100

    added collab upload_file

--- driver.py
+++ driver.py
@@ -3,15 +3,17 @@
 import pyjugex
 import hbp_human_atlas as atlas
 import nibabel as nib
-import collab
+#UNCOMMENT THE FOLLOWING TWO LINES UPLOAD THE PMAPS TO COLLAB STORAGE
+#import collab
 
 genelist = ['ADRA2A', 'AVPR1B', 'CHRM2', 'CNR1', 'CREB1', 'CRH', 'CRHR1', 'CRHR2', 'GAD2', 'HTR1A', 'HTR1B', 'HTR1D', 'HTR2A', 'HTR3A', 'HTR5A', 'MAOA', 'PDE1A', 'SLC6A2', 'SLC6A4', 'SST', 'TAC1', 'TPH1', 'GPR50', 'CUX2', 'TPH2']
 #genelist = ['ADRA2A', 'AVPR1B', 'CHRM2']
 roi1 = atlas.jubrain.probability_map('FP1', atlas.MNI152)
 roi2 = atlas.jubrain.probability_map('FP2', atlas.MNI152)
-collab.collab_pyjugex.upload(roi1['name'])
-collab.collab_pyjugex.upload(roi2['name'])
-jugex = pyjugex.PyJugex(cache=".pyjugex", verbose=False)
+#UNCOMMENT THE FOLLOWING TWO LINES UPLOAD THE PMAPS TO COLLAB STORAGE
+#collab.upload(roi1['name'])
+#collab.upload(roi2['name'])
+jugex = pyjugex.PyJugex(cache=".pyjugex", verbose=True)
 result = jugex.DifferentialAnalysis(genelist, roi1['image'], roi2['image'])
 if len([id for id in result if result[id] < .05]) > 0:
     print('Differentially expressed genes are : ')
