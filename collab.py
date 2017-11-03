239153de65c3f4b9d73487c0a57d813b1b7404a3
haimasree <hbhattacharya@fz-juelich.de>
2017-11-03 16:29:54 +0100

    change verbose flags

--- collab.py
+++ collab.py
@@ -2,17 +2,20 @@
 # -*- coding: utf-8 -*-
 
 def upload(filename):
-    client = get_hbp_service_client()
+    clients = get_hbp_service_client()
     collab_path = get_collab_storage_path()
     if not clients.storage.exists(collab_path + '/pmaps'):
-        pmap_folder = client.storage.mkdir(collab_path + '/pmaps')
+        pmap_folder = clients.storage.mkdir(collab_path + '/pmaps')
     else:
         pmap_folder = collab_path + '/pmaps'
+    print(pmap_folder)
     ufilename = collab_path + '/pmaps/' + filename
+    if clients.storage.exists(ufilename):
+        clients.storage.delete(ufilename)
     if filename.split('.')[-1] == 'gz':
-        client.storage.upload_file(filename, ufilename, 'image/gznii')
+        clients.storage.upload_file(filename, ufilename, 'image/gznii')
     elif filename.split('.')[-1] == 'nii':
-        client.storage.upload_file(filename, ufilename, 'image/nii')
+        clients.storage.upload_file(filename, ufilename, 'image/nii')
     else:
         print('Not a valid file format')
         exit()
