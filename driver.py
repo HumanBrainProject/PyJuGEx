5fdf864806808ee71c8cbcd1836b030aba44d9a3
haimasree <hbhattacharya@fz-juelich.de>
2017-11-17 14:57:52 +0100

    changed driver.py

--- driver.py
+++ driver.py
@@ -7,7 +7,6 @@ import hbp_human_atlas as atlas
 
 genelist = ['ADRA2A', 'AVPR1B', 'CHRM2', 'CNR1', 'CREB1', 'CRH', 'CRHR1', 'CRHR2', 'GAD2', 'HTR1A', 'HTR1B', 'HTR1D', 'HTR2A', 'HTR3A', 'HTR5A', 'MAOA', 'PDE1A', 'SLC6A2', 'SLC6A4', 'SST', 'TAC1', 'TPH1', 'GPR50', 'CUX2', 'TPH2']
 #genelist = ['ADRA2A', 'AVPR1B', 'CHRM2']
-
 roi1 = atlas.jubrain.probability_map('FP1', atlas.MNI152)
 roi2 = atlas.jubrain.probability_map('FP2', atlas.MNI152)
 
