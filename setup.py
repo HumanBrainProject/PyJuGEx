a9503d2f13713438378ca35b50b733894eb98fdb
Haimasree Bhattacharya <31005140+haimasree@users.noreply.github.com>
2017-12-12 10:51:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 setup(name='pyjugex',
       version='0.1',
       description='Perform differential gene expression on two chosen brain regions' ,
+      author='Haimasree Bhattacharya',
+      author_email='h.bhattacharya@fz-juelich.de',
       py_modules=['pyjugex', 'analysispyjugex', 'hbp_human_atlas'],
       install_requires=['numpy', 'scipy', 'statsmodels', 'requests', 'nibabel', 'xmltodict',], 
       )
