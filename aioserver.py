503e3cfba17676ce52cb7e9adba53a994dd459c6
haimasree <hbhattacharya@fz-juelich.de>
2017-12-14 16:03:13 +0100

    added aioserver.py

---
+++ aioserver.py
@@ -0,0 +1,45 @@
+from aiohttp import web
+import aiohttp_cors
+import json
+import nibabel as nib
+import hbp_human_atlas as atlas
+import analysispyjugex
+
+with open("genesymbols.txt", "r") as f:
+    dictAutocompleteString = f.read()
+#dictAutocomplete = ["ADRA2A", "AVPR1B", "CHRM2", "CNR1", "CREB1", "CRH", "CRHR1", "CRHR2", "GAD2", "HTR1A", "HTR1B", "HTR1D", "HTR2A", "HTR3A", "HTR5A", "MAOA", "PDE1A", "SLC6A2", "SLC6A4", "SST", "TAC1", "TPH1", "GPR50", "CUX2", "TPH2"]
+
+def pyjugex_analysis(jsonobj):
+    print(jsonobj)
+    roi1 = {}
+    roi2 = {}
+    roi1['data'] = atlas.jubrain.probability_map(jsonobj['area1']['url'], jsonobj['area1']['name'], atlas.MNI152)
+    roi1['name'] = jsonobj['area1']['name']
+    roi2['data'] = atlas.jubrain.probability_map(jsonobj['area2']['url'], jsonobj['area2']['name'], atlas.MNI152)
+    roi2['name'] = jsonobj['area2']['name']
+    jugex = analysispyjugex.Analysis(gene_cache_dir=".pyjugex", verbose=False)
+    result = jugex.DifferentialAnalysis(jsonobj['genelist'], roi1, roi2)
+    return result
+
+async def handle_post(request):
+    if request.has_body:
+        jsonobj = await request.json()
+    else:
+        return web.Response(status=400)
+    data = pyjugex_analysis(jsonobj)
+    return web.Response(status=200,content_type="application/json",body=data)
+
+async def return_auto_complete(request):
+#    return web.Response(status=200,content_type="application/json",body=json.dumps(dictAutocomplete))
+     return web.Response(status=200,content_type="application/json",body=dictAutocompleteString)
+
+
+def main():
+    app = web.Application()
+    cors = aiohttp_cors.setup(app)
+    cors.add(app.router.add_post("/jugex",handle_post), {"*": aiohttp_cors.ResourceOptions(expose_headers="*", allow_headers="*")})
+    cors.add(app.router.add_get("/",return_auto_complete), {"*": aiohttp_cors.ResourceOptions(expose_headers="*", allow_headers="*")})
+    web.run_app(app,host="0.0.0.0",port=8003)
+
+if __name__=='__main__':
+    main()
