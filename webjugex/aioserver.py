7a7e3f31863dc128ee33d48075540f96203d2d6e
Stefan Köhnen <s.koehnen@fz-juelich.de>
2021-01-22 16:34:27 +0100

    Extract transformation of jugex response to separate function

--- webjugex/aioserver.py
+++ webjugex/aioserver.py
@@ -42,6 +42,55 @@ with open("files/genesymbols.txt", "r") as f:
 
 token_handler = jwt_handler.jwt_handler()
 
+def _transform_brainscapes_response(brainscapes_resp, jsonobj):
+    zscores = brainscapes_resp["zscores"][jsonobj['selectedGenes'][0]]
+
+    for gene in jsonobj['selectedGenes'][1:]:
+        zscores = zip(zscores, brainscapes_resp["zscores"][gene])
+
+    probes_area1 = []
+    probes_area2 = []
+
+    brainscapes_areas = list(set(brainscapes_resp["area"]))
+
+    for i in range(len(list(brainscapes_resp["zscores"].values())[0])):
+        tmp_probe = {"probe_properties": {}}
+        for gene in jsonobj['selectedGenes']:
+            tmp_probe["probe_properties"][gene] = brainscapes_resp["zscores"][gene][i]
+
+        tmp_probe["position"] = brainscapes_resp["mnicoord"][i]
+
+        if brainscapes_resp["area"][i] == brainscapes_areas[0]:
+            print("Area 1")
+            probes_area1.append(tmp_probe)
+        else:
+            print("Area 2")
+            probes_area2.append(tmp_probe)
+
+    print(probes_area1)
+    print(probes_area2)
+
+    result = {"result": brainscapes_resp["p-values"]}
+    #result["Version"] = os.environ["OPENSHIFT_BUILD_COMMIT"]
+    result["Areas"] = []
+    result["Areas"].append(
+                        {
+                            "name": jsonobj["area1"]["areas"][0]["name"],
+                            "hemisphere": jsonobj["area1"]["areas"][0]["hemisphere"],
+                            "probes": probes_area1
+                        })
+    result["Areas"].append(
+                    {
+                        "name": jsonobj["area2"]["areas"][0]["name"],
+                        "hemisphere": jsonobj["area2"]["areas"][0]["hemisphere"],
+                        "probes": probes_area2
+                    })
+
+    print(result)
+
+    return result
+
+
 def run_pyjugex_analysis(jsonobj):
     print(jsonobj)
     filter_threshold = jsonobj.get('threshold', default_param['threshold'])
@@ -80,50 +129,7 @@ def run_pyjugex_analysis(jsonobj):
 
     logger.log("info", {"jugex_result": str(jugex_result)})
 
-    zscores = jugex_result["zscores"][jsonobj['selectedGenes'][0]]
-
-    for gene in jsonobj['selectedGenes'][1:]:
-        zscores = zip(zscores, jugex_result["zscores"][gene])
-
-    probes_area1 = []
-    probes_area2 = []
-
-    brainscapes_areas = list(set(jugex_result["area"]))
-
-    for i in range(len(list(jugex_result["zscores"].values())[0])):
-        tmp_probe = {"probe_properties": {}}
-        for gene in jsonobj['selectedGenes']:
-            tmp_probe["probe_properties"][gene] = jugex_result["zscores"][gene][i]
-
-        tmp_probe["position"] = jugex_result["mnicoord"][i]
-
-        if jugex_result["area"][i] == brainscapes_areas[0]:
-            print("Area 1")
-            probes_area1.append(tmp_probe)
-        else:
-            print("Area 2")
-            probes_area2.append(tmp_probe)
-
-    print(probes_area1)
-    print(probes_area2)
-
-    result = {"result": jugex_result["p-values"]}
-    #result["Version"] = os.environ["OPENSHIFT_BUILD_COMMIT"]
-    result["Areas"] = []
-    result["Areas"].append(
-                        {
-                            "name": jsonobj["area1"]["areas"][0]["name"],
-                            "hemisphere": jsonobj["area1"]["areas"][0]["hemisphere"],
-                            "probes": probes_area1
-                        })
-    result["Areas"].append(
-                    {
-                        "name": jsonobj["area2"]["areas"][0]["name"],
-                        "hemisphere": jsonobj["area2"]["areas"][0]["hemisphere"],
-                        "probes": probes_area2
-                    })
-
-    print(result)
+    result = _transform_brainscapes_response(jugex_result, jsonobj)
 
     logger.log("info", {"returned_result": str(result)})
 
--- webjugex/test_webjugex.py
+++ webjugex/test_webjugex.py
@@ -12,6 +12,55 @@ token_handler = jwt_handler.jwt_handler()
 time.sleep(5)
 print("token service started")
 
+def _transform_brainscapes_response(brainscapes_resp, jsonobj):
+    zscores = brainscapes_resp["zscores"][jsonobj['selectedGenes'][0]]
+
+    for gene in jsonobj['selectedGenes'][1:]:
+        zscores = zip(zscores, brainscapes_resp["zscores"][gene])
+
+    probes_area1 = []
+    probes_area2 = []
+
+    brainscapes_areas = list(set(brainscapes_resp["area"]))
+
+    for i in range(len(list(brainscapes_resp["zscores"].values())[0])):
+        tmp_probe = {"probe_properties": {}}
+        for gene in jsonobj['selectedGenes']:
+            tmp_probe["probe_properties"][gene] = brainscapes_resp["zscores"][gene][i]
+
+        tmp_probe["position"] = brainscapes_resp["mnicoord"][i]
+
+        if brainscapes_resp["area"][i] == brainscapes_areas[0]:
+            print("Area 1")
+            probes_area1.append(tmp_probe)
+        else:
+            print("Area 2")
+            probes_area2.append(tmp_probe)
+
+    print(probes_area1)
+    print(probes_area2)
+
+    result = {"result": brainscapes_resp["p-values"]}
+    #result["Version"] = os.environ["OPENSHIFT_BUILD_COMMIT"]
+    result["Areas"] = []
+    result["Areas"].append(
+                        {
+                            "name": jsonobj["area1"]["areas"][0]["name"],
+                            "hemisphere": jsonobj["area1"]["areas"][0]["hemisphere"],
+                            "probes": probes_area1
+                        })
+    result["Areas"].append(
+                    {
+                        "name": jsonobj["area2"]["areas"][0]["name"],
+                        "hemisphere": jsonobj["area2"]["areas"][0]["hemisphere"],
+                        "probes": probes_area2
+                    })
+
+    print(result)
+
+    return result
+
+
 def run_pyjugex_analysis(jsonobj):
     print(jsonobj)
     filter_threshold = jsonobj.get('threshold', default_param['threshold'])
@@ -35,8 +84,6 @@ def run_pyjugex_analysis(jsonobj):
     print(area1_julich_brain_version)
 
     atlas.select_parcellation(brainscapes.parcellations.JULICH_BRAIN_PROBABILISTIC_CYTOARCHITECTONIC_MAPS_V2_5_)
-    # as in the original JuGEx, we prefer thresholded probability maps # over the labelled region in the maximum probability map
-    #atlas.enable_continuous_map_thresholding(filter_threshold)
 
     jugex = brainscapes.analysis.DifferentialGeneExpression(atlas)
 
@@ -46,71 +93,13 @@ def run_pyjugex_analysis(jsonobj):
     jugex.define_roi1(jsonobj["area1"]["areas"][0]["name"] + " " + jsonobj["area1"]["areas"][0]["hemisphere"])
     jugex.define_roi2(jsonobj['area2']["areas"][0]["name"] + " " + jsonobj["area2"]["areas"][0]["hemisphere"])
 
-    #from nilearn import plotting import numpy as np
-    #for region,samples in zip(['v1 right','v2 right'],[jugex.samples1,jugex.samples2]):
-    #atlas.select_region(region)
-    #mask = atlas.get_mask(bs.spaces.MNI_152_ICBM_2009C_NONLINEAR_ASYMMETRIC) display = plotting.plot_roi(mask)
-    #display.add_markers([k for k,v in samples.items()])
-
-    #for item in jugex._samples1.items():
-    #    print(item[1])
-    #    for item_1 in item[1]:
-    #        print(item_1)
-    #        print(item[1][item_1])
-    #        print(type(item[1][item_1]))
-
     jugex.run(permutations=n_rep)
     jugex_result = jugex.result()
     print(jugex_result)
 
     #logger.log("info", {"jugex_result": str(jugex_result)})
 
-    zscores = jugex_result["zscores"][jsonobj['selectedGenes'][0]]
-
-    for gene in jsonobj['selectedGenes'][1:]:
-        zscores = zip(zscores, jugex_result["zscores"][gene])
-
-    #"probes":[{"probe_properties": {"MAOA": 0.0101010, "TAC1": -1232434}, "position": [x,y,z]
-
-    probes_area1 = []
-    probes_area2 = []
-
-    brainscapes_areas = list(set(jugex_result["area"]))
-
-    for i in range(len(list(jugex_result["zscores"].values())[0])):
-        tmp_probe = {"probe_properties": {}}
-        for gene in jsonobj['selectedGenes']:
-            tmp_probe["probe_properties"][gene] = jugex_result["zscores"][gene][i]
-
-        tmp_probe["position"] = jugex_result["mnicoord"][i]
-
-        if jugex_result["area"][i] == brainscapes_areas[0]:
-            print("Area 1")
-            probes_area1.append(tmp_probe)
-        else:
-            print("Area 2")
-            probes_area2.append(tmp_probe)
-
-    print(probes_area1)
-    print(probes_area2)
-
-    result = {"result": jugex_result["p-values"]}
-    #result["Version"] = os.environ["OPENSHIFT_BUILD_COMMIT"]
-    result["Areas"] = []
-    result["Areas"].append(
-                        {
-                            "name": jsonobj["area1"]["areas"][0]["name"],
-                            "hemisphere": jsonobj["area1"]["areas"][0]["hemisphere"],
-                            "probes": probes_area1
-                        })
-    result["Areas"].append(
-                    {
-                        "name": jsonobj["area2"]["areas"][0]["name"],
-                        "hemisphere": jsonobj["area2"]["areas"][0]["hemisphere"],
-                        "probes": probes_area2
-                    })
-
-    print(result)
+    result = _transform_brainscapes_response(jugex_result, jsonobj)
 
     #logger.log("info", {"returned_result": str(result)})
 
